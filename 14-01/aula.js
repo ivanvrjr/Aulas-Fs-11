ea47f02691b911be8c10ef356afd07f8963f02f5
Ivan Vieira <ivanvrjr@hotmail.com>
2023-01-21 11:26:14 -0300

    arquivo aula dia 14 de jan

---
+++ 14-01/aula.js
@@ -0,0 +1,20 @@
+var bomDia = "Bom dia, "
+ var valida = "positivo"
+alert ('valida')
+
+var nome = "Ivan"
+alert(typeof nome)
+
+var idade = 27
+alert(typeof idade)
+
+var trabalho = true
+alert(typeof trabalho)
+
+var sobrenome = ("Vieira")
+var nomeCompleto = nome+ " " + sobrenome
+alert (bomDia + nomeCompleto)
+
+alert (`Bom dia ${nomeCompleto}, sua idade é ${idade} anos`)
+
+
