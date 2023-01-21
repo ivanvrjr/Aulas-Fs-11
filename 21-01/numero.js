90c99b5e24a952d8a1cb6485797123e0b7712464
Ivan Vieira <ivanvrjr@hotmail.com>
2023-01-21 12:08:41 -0300

    Alteração aula

--- 21-01/numero.js
+++ 21-01/numero.js
@@ -1,7 +1,38 @@
-const idade=prompt("Qual sua idade?")
+/*const idade=prompt("Qual sua idade?")
 if(idade >= 18){
     alert("Você é de maior")
 }
 else{
     alert("você é de menor")
+}
+*/
+/*
+const idade=prompt ("Me diga sua idade")
+if(idade >= 18){
+    alert("você é maior de idade")
+}
+else if(idade >=14){
+    alert("você é adolescente")
+}
+else{
+    alert ("você é criança")
+}
+*/
+const altura=prompt ("Me diga sua altura")
+const peso=prompt ("Me diga o seu peso")
+var IMC = peso/altura**2
+if(IMC < 18.5){
+    alert("você é classificado como magro, valor= " + IMC)
+}
+else if(IMC <= 24.9){
+    alert("você é classificado como normal, valor=" +  IMC)
+}
+else if(IMC <= 29.9){
+    alert("você é classificado com sobrepeso, valor=" +  IMC)
+}
+else if(IMC <= 39.9){
+    alert("você é classificado com obesidade, valor=" +  IMC)
+}
+else{
+    alert("você é classificado com obesidade grave, valor=" +  IMC)
 }
